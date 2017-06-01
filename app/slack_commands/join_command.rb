2cd068b04ab5b57628e550b208afaa1e33231dc7
Randall Elzinga <rjelzinga@gmail.com>
2017-06-01 17:13:28 -0400

    Create join_command.

---
+++ app/slack_commands/join_command.rb
@@ -0,0 +1,16 @@
+class JoinCommand < CommandsController
+
+  def add(params)
+    tournament_id = params[:text]
+    if tournament = Tournament.find_by(id: tournament_id)
+      if Player.create(name: params[:user_name], tournament_id: tournament.id)
+        render_to_user(':white_check_mark: Thanks! You have joined the tournament!')
+      else
+        render_to_user(ErrorsHelper.save_error)
+      end
+    else
+      render_to_user(':no_good: That\'s not a valid tournament id. Enter "/tournament-list" to see a list of tournaments you can join')
+    end
+  end
+
+end
